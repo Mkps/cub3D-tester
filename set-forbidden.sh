1fcada552cb4c5ab08c40d7cdee86b24ad007777
Alexis LOUBIERE <aloubier@student.42.fr>
2024-03-11 12:23:46 +0100

    created script to make items forbidden. fixed map path in script

---
+++ set-forbidden.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Making files forbidden for the test"
+chmod 000 ./maps/bad/forbidden.cub
+chmod 000 ./textures/test/forbidden.xpm
--- test-map-bad.sh
+++ test-map-bad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 execPath="cub3D"
 makePath="."
-mapsPath='cub3d-tester/maps/bad/'
+mapsPath='cub3D-tester/maps/bad/'
 
 termwidth="$(tput cols)"
 green='\e[1;32m'
